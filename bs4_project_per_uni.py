4722b116ae2c19fbe40df736625a01b7b58f561d
Ivan Emoje <emojeivan@gmail.com>
2017-06-25 11:51:02 +0300

    Update and rename bs4_project_phase1.py to bs4_project_per_uni.py

---
+++ bs4_project_per_uni.py
@@ -0,0 +1,15 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+
+#open page
+page=urllib2.urlopen('http://www.4icu.org/dz/')
+
+#parse page using bs4
+soup = BeautifulSoup(page, 'html.parser')
+
+#print uni_names
+for tr in soup.find_all('tr')[1:]:
+    tds = tr.find_all('td')
+    print tds[1].text
+   
