acd79b5c73753357851f88075a2de8040ed4dd99
Ivan Emoje <emojeivan@gmail.com>
2017-06-26 17:07:50 +0300

    Update bs4_part_3.py

--- bs4_part_3.py
+++ bs4_part_3.py
@@ -1,15 +1,25 @@
 import urllib2
 from bs4 import BeautifulSoup
 
+#open page and parse it for use to get links for all countries
 page=urllib2.urlopen('http://www.4icu.org/Africa')
 soup = BeautifulSoup(page, 'html.parser')
+
+#string that will be appended to hyperlinks of countries
 str="http://www.4icu.org"
+
+#array list that will hold links
 data=[]
+
+#for loop to obtain links for all countries
 for link in soup.find_all('a'):
    data.append(str+link.get('href'))
 data = [d.replace('/index.html', "/") for d in data]
+
+#new array that obtains links from africa -- rudimentary method
 africa=data[40:92] 
 
+#failed attempt to iterate over each link and add to array/insert into db
 for i in range(len(africa)):
     link=africa[i]
     page=urllib2.urlopen("'"+link+"'")
