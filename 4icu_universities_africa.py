862cdcc943485bba16c392166310b8ea0f517215
Ivan Emoje <emojeivan@gmail.com>
2017-06-28 11:34:58 +0300

    Update and rename bs4_part_5_complete.py to 4icu_universities_africa.py

--- 4icu_universities_africa.py
+++ 4icu_universities_africa.py
@@ -1,7 +1,14 @@
+#Extract names of all universities in Africa using bs4
+#Generate CSV, PDF of stored data from mySQL
+
 
 import urllib2
 from bs4 import BeautifulSoup
 import pymysql
+import csv
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Spacer, Table, TableStyle
 
 #open connection and create cursor object
 conn=pymysql.connect(host='localhost', port=3306, user='root', passwd='1234',db='4icu')
@@ -44,12 +51,26 @@ for i in range(len(africa)):
         tds = tr.find_all('td')
         params=[tds[1].text, country]
         cur.execute("""INSERT INTO university_africa VALUES (%s,%s);""",params)
-      #write to csv file
-        with open("/home/ivan/Documents/uni_csv.csv", "a") as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerow([params, datetime.now()])
     conn.commit() 
 print "complete"
 
+#Query Data
+cur.execute("""SELECT * FROM uni_test';""")
+universities=cur.fetchall()
+print data
 
+#write into CSV
+c = csv.writer(open("/uni_csv.csv","wb"))
+for row in data:
+    c.writerow(row)
 
+#write into PDF
+
+doc = SimpleDocTemplate("uni_pdf.pdf", pagesize=letter, showBoundary=1)
+parts = []
+#populate table in pdf with data from mysql
+table = Table(data, [5 * inch, 0.5 * inch, inch])
+parts.append(table)
+parts.append(Spacer(1, 1.5 * inch))
+#build doc
+doc.build(parts)
